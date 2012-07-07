e3ea5f8f6f98f54044f2159204dc02a64241eae9
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-07-07 16:20:34 +0400

    added code line

--- lib/crazytest/standard_reporter.rb
+++ lib/crazytest/standard_reporter.rb
@@ -11,26 +11,48 @@ module Crazytest
     end
 
     def print_report(result_object)
-      puts
-      puts
+      2.times do; puts end
       i = 1
       result_object.fail_result.each do |result, method|
+        # title
         puts "#{i}) #{method.name.to_s.yellow.bold} (#{method.source_location.join(', ')})"
+
+        # message
         if result.message.empty?
-          puts "Message: no message given"
+          puts "Message: `no message given`"
         else
-          puts "Message: #{result.message}"
+          puts "Message: `#{result.message}`"
         end
 
+        # code
+        line_number = result.backtrace[1].match('/*:(\d+)/*')[1].to_i
+        if line_number > 0
+          puts "Code:"
+          code = read_line(method.source_location.first, line_number)
+          puts "#{line_number}: #{code}".cyan
+        end
+
+        # trace
         unless result.is_a? FailError
           puts "Trace: #{result.backtrace.join("\n")}"
         end
+
         i += 1
       end
-      puts
+
+      # summary
       puts "Summary".blue.underline
       puts "Ok      : #{result_object.ok_count}".green
       puts "Failures: #{result_object.fail_count}".red
+    end
+
+    private
+
+    def read_line(filename, line_number)
+      f = File.open(filename)
+      line =  [*f][line_number-1]
+      f.close
+      line
     end
   end
 end
